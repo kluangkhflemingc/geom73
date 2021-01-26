35561e80e2a11b55e92ef06770ee6e0f936937a3
JenniferDebono <72082183+JenniferDebono@users.noreply.github.com>
2021-01-26 13:51:17 -0500

    added rough work file

---
+++ roughOut.py
@@ -0,0 +1,10 @@
+Work Plan
+Script 1
+Workspace: input folder 
+Step 1: Count & Return
+-	Total number shapefiles
+-	Total number excel files
+Step 2:
+For each file: 
+if shp report number of features
+if table report total number of rows
